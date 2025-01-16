184fe1049d1d4a1bce991fc5478a2913d816d2a5
shahan12 <shahankhan485@gmail.com>
2025-01-16 06:32:37 +0530

    bug fix

--- longestPali.js
+++ longestPali.js
@@ -2,7 +2,7 @@ function findPali(str) {
   let max = "";
   for (let i = 0; i < str.length; i++) {
     let oddPali = findOddPali(i, i, str);
-    let evenPali = findOddPali(i + 1, i, str);
+    let evenPali = findOddPali(i - 1, i, str);
     if (max.length < oddPali.length) max = oddPali;
     if (max.length < evenPali.length) max = evenPali;
   }
